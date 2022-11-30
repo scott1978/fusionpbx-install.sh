eba35a4f2d85a0117d4308556497c4752999ebdd
fusionpbx-cn <33116308+scott1978@users.noreply.github.com>
2022-11-30 12:32:20 +0800

    Update package-release.sh

--- centos/resources/switch/package-release.sh
+++ centos/resources/switch/package-release.sh
@@ -14,8 +14,10 @@ verbose "Installing FreeSWITCH"
 yum -y install memcached curl gdb
 
 #install freeswitch packages
-yum install -y https://files.freeswitch.org/repo/yum/centos-release/freeswitch-release-repo-0-1.noarch.rpm epel-release
-yum install -y freeswitch-config-vanilla freeswitch-lang-* freeswitch-sounds-* freeswitch-lua freeswitch-xml-cdr
+echo "scott" > /etc/yum/vars/signalwireusername
+echo "pat_2mLLY6czvUx4F8U2ChrGwQ59" > /etc/yum/vars/signalwiretoken
+yum install -y https://$(< /etc/yum/vars/signalwireusername):$(< /etc/yum/vars/signalwiretoken)@freeswitch.signalwire.com/repo/yum/centos-dev/freeswitch-dev-repo-0-1.noarch.rpm epel-release
+yum install -y freeswitch-config-vanilla freeswitch-lang-en* freeswitch-sounds-en* freeswitch-lua freeswitch-xml-cdr
 
 #remove the music package to protect music on hold from package updates
 mkdir -p /usr/share/freeswitch/sounds/temp
