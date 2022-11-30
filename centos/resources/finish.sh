4f22d28ccd5148065134ef69b780abb3814fc021
fusionpbx-cn <33116308+scott1978@users.noreply.github.com>
2022-11-30 13:58:09 +0800

    Update finish.sh

--- centos/resources/finish.sh
+++ centos/resources/finish.sh
@@ -23,7 +23,7 @@ sudo -u postgres /usr/bin/psql -c "ALTER USER fusionpbx WITH PASSWORD '$database
 sudo -u postgres /usr/bin/psql -c "ALTER USER freeswitch WITH PASSWORD '$database_password';"
 
 #add the config.conf
-cp fusionpbx/config.conf /etc/fusionpbx
+cp fusionpbx/config.conf /etc/fusionpbx/config.conf
 sed -i /etc/fusionpbx/config.conf -e s:"{database_host}:$database_host:"
 sed -i /etc/fusionpbx/config.conf -e s:"{database_name}:$database_name:"
 sed -i /etc/fusionpbx/config.conf -e s:"{database_username}:$database_username:"
